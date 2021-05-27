e455094b5d201530135e68a9029650176cee8320
Sanchit Kashyap <sanchitkshyap@gmail.com>
2021-05-27 09:02:52 -0700

    Update CountryPicker.js

--- src/Components/CountryPicker/CountryPicker.js
+++ src/Components/CountryPicker/CountryPicker.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { NativeSelect, FormControl} from '@material-ui/core' 
-import styles from './CountryPicker.module.css'
+
 
 import { fetchCountries } from '../../api';
 
